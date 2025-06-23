6e0e301ed5b56ac5e6c855e1056783096abb8569
Michu <michu9139@gmail.com>
2025-06-23 19:55:45 +0200

    Handling Events
    -welcome
    -word filter

--- Discord_Bot/main.py
+++ Discord_Bot/main.py
@@ -14,3 +14,23 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+@bot.event
+async def on_ready():
+    print(f"We are ready to go in, {bot.user.name}")  
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server {member.name}")
+    
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return 
+    
+    if "shit" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} - don't use that word nigger!")
+
+        await bot.process_commands(message)
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
